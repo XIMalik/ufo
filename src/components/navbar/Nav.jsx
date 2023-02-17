6d97f931e174c06b004243c7db6c54d4fe9be6b2
XIMalik <abdulmalikawesu@gmail.com>
2023-02-17 09:09:13 +0100

    Trey

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -19,11 +19,11 @@ export default function Nav(){
     <div className="navWrapper p-5 w-[100%] max-w-[1024px]">
         <div className="navbar flex  justify-between align-center items-center">
             <h1  onClick={goHome} className="logo text-white text-xs hover:text-green-100">Malik Awesu</h1>
-            <div className="nav-text flex gap-4 text-gray-300">
+            {/* <div className="nav-text flex gap-4 text-gray-300">
                 <span onClick={goAbout} className="ease-out duration-500 hover:underline hover:text-green-100 underline-offset-8">Work</span>
                 <span onClick={goAbout} className="ease-out duration-300 hover:underline hover:text-green-100 underline-offset-8">About</span>
                 <span onClick={goAbout} className="ease-out duration-500 hover:underline hover:text-green-100 underline-offset-8">Contact</span>
-            </div>
+            </div> */}
         </div>
     </div>
    </div>
