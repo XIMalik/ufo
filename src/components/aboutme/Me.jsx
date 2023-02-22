f5f96aba29f1e814b0cd803ebac7160739357bfa
XIMalik <abdulmalikawesu@gmail.com>
2023-02-22 17:52:26 +0100

    red

--- src/components/aboutme/Me.jsx
+++ src/components/aboutme/Me.jsx
@@ -17,7 +17,7 @@ export default function Me (){
       <div className='sm:flex justify-center items-center w-[100%] max-w-[1024px] px-10 sm:py-0 pt-5  bg-gradient-to-r from-green-100 to-blue-100'>
         <div className='flex flex-col gap-3 text-center sm:text-left'>
           <h1 className='heroText text-5xl'>Hello world</h1>
-          <p>I am a Frontend Developer and UI Designer passionate about building and designing products to enhance businesses and individuals</p>
+          <p>I am a Frontend Developer and Product Designer passionate about building and designing products to enhance businesses and individuals</p>
           <button onClick={goHome} className='bg-white ease-in duration-150 text-xs hover:text-green-100 hover:bg-slate-800 px-5 py-3 rounded-lg sm:w-[60%] sm:text-center'>View my resume</button>
         </div>
         <img src={avatar} alt="" className='w-[50%] mx-auto'/>
