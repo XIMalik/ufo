eeb7e754de55f39377a0434885758f12c4ee9d4f
Lekan <77983247+XIMalik@users.noreply.github.com>
2024-08-11 20:13:05 +0100

    Update Nav.jsx

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -18,7 +18,7 @@ export default function Nav(){
    <div className="navContainer z-40 flex justify-center w-[100%] max-w-[1024] sticky top-0 mb-2 border-gray-700 border-solid">
     <div className="navWrapper p-5 w-[100%] max-w-[1024px]">
         <div className="navbar flex  justify-center align-center items-center">
-            <h1  onClick={goHome} className="logo text-white text-xs hover:text-green-100">Malik Awesu</h1>
+            <h1  onClick={goHome} className="logo text-green-600 text-xs ease-in duration-150 hover:text-green-100">Malik Awesu</h1>
         </div>
     </div>
    </div>
